ad60750e87987181835d3e34054f37a77be01bf3
hham2 <henrikham14@gmail.com>
2024-05-27 18:44:49 -0400

    output tests failed but they print what was asked?

--- practice_input.py
+++ practice_input.py
@@ -11,6 +11,10 @@ def get_favorite_vegetable():
     where X is replaced with the user's favorite vegetable.
     """
     # write your code here.
+    favorite_vegetable = input("what is your favorite vegetable?")
+    first = "Interesting! I also love "
+    last = "!"
+    print(first, favorite_vegetable, last, sep="")
 
 
 def get_favorite_number():
@@ -20,6 +24,10 @@ def get_favorite_number():
     where X is replaced with the user's favorite number.
     """
     # write your code here.
+    favorite_number = input("what is your favorite number?")
+    first = "Interesting! I also love "
+    last = "!"
+    print(first, favorite_number, last, sep="")
 
 
 def get_name_and_zodiac_sign():
@@ -30,6 +38,12 @@ def get_name_and_zodiac_sign():
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
     # write your code here.
+    name = input("what is your name?")
+    sign = input("what is your zodiac sign?")
+    first = "Interesting! My name is also "
+    second = ", and I'm also a "
+    last = "!"
+    print(first, name, second, sign, last, sep="")
 
 
 def get_name_and_age():
@@ -40,3 +54,9 @@ def get_name_and_age():
     where X and Y are replaced by the user's name and age, respectively.
     """
     # write your code here.
+    name = input("what is your name?")
+    age = input("what is your age?")
+    first = "Interesting! My name is also "
+    second = ", and I'm also "
+    last = " years old!"
+    print(first, name, second, age, last, sep="")
