8bf202bf9f1b07939af541b847bad4c510393592
hham2 <henrikham14@gmail.com>
2024-05-27 20:52:58 -0400

    5.27

--- practice_output.py
+++ practice_output.py
@@ -10,7 +10,7 @@ def print_with_line_break():
     Prints out the text, 'Hello world!' with a line break at the end
     """
     # write your code here
-    print("Hello world!\n")
+    print("Hello world!")
 
 
 def print_without_line_break():
@@ -18,7 +18,7 @@ def print_without_line_break():
     Prints out the text, 'Hello world!' without a line break at the end
     """
     # write your code here'
-    print("Hello world!")
+    print("Hello world!", end="")
 
 
 def print_with_separator_dash_and_with_line_break():
@@ -29,7 +29,7 @@ def print_with_separator_dash_and_with_line_break():
     """
     # write your code here
     # you must supply each word as a separate argument to the print() function
-    print("Twas" + "-" + "brillig" + "-" + "and" + "-" + "the" + "-" + "slithy" + "-" + "toves\n")
+    print("Twas", "-", "brillig", "-", "and", "-", "the", "-", "slithy", "-", "toves\n", sep="")
 
 def print_with_separator_dash_and_without_line_break():
     """
